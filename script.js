f86cfa6a598232f7ff495eb5e9fa252cb2cc4a49
Ishaan Kalra <74290686+IshaanKalra2103@users.noreply.github.com>
2023-07-08 20:43:00 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const API_KEY = process.env.API_KEY;
+const API_URL =
+  "https://api.openai.com/v1/engines/text-davinci-003/completions";
+
+let chatbox = document.getElementById("chat-box");
+let prompt = document.getElementById("chat-input").value;
+
+async function getCompletion(prompt) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      prompt: prompt,
+      max_tokens: 1000,
+      temperature: 0,
+    }),
+  });
+  const data = await response.json();
+  result = data.choices[0].text.trim();
+  loadData(result);
+}
+
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", () => {
+  const promptInput = document.getElementById("chat-input");
+  const prompt = promptInput.value.trim();
+
+  if (prompt === "") {
+    alert("Enter Some Text!");
+  } else {
+    // Append user's message to chatbox
+    chatbox.innerHTML += "<p><strong>You:</strong> " + prompt + "</p>";
+  }
+
+  getCompletion(prompt);
+});
+
+function loadData(content) {
+  chatbox.innerHTML += "<p><strong>Bot:</strong> " + content + "</p>";
+}
